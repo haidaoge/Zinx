5f3f36b50903e0a9daab78fb005cccd225facb23
haidaoge <suohaitao@jiyiuav.com>
2019-11-06 18:50:37 +0800

    消息队列及多任务

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -12,14 +12,27 @@ import (
    一些参数也可以通过 用户根据 zinx.json来配置
 */
 type GlobalObj struct {
+	/*
+		Server
+	*/
 	TcpServer ziface.IServer //当前Zinx的全局Server对象
 	Host      string         //当前服务器主机IP
 	TcpPort   int            //当前服务器主机监听端口号
 	Name      string         //当前服务器名称
-	Version   string         //当前Zinx版本号
 
-	MaxPacketSize uint32 //都需数据包的最大值
-	MaxConn       int    //当前服务器主机允许的最大链接个数
+	/*
+		Zinx
+	*/
+	Version          string //当前Zinx版本号
+	MaxPacketSize    uint32 //读取数据包的最大值
+	MaxConn          int    //当前服务器主机允许的最大链接个数
+	WorkerPoolSize   uint32 //业务工作Worker池的数量
+	MaxWorkerTaskLen uint32 //业务工作Worker对应负责的任务队列最大任务存储数量
+
+	/*
+		config file path
+	*/
+	ConfFilePath string
 }
 
 /*
@@ -29,7 +42,7 @@ var GlobalObject *GlobalObj
 
 //读取用户的配置文件
 func (g *GlobalObj) Reload() {
-	data, err := ioutil.ReadFile("../../conf/zinx.json")
+	data, err := ioutil.ReadFile(g.ConfFilePath)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -48,12 +61,15 @@ func (g *GlobalObj) Reload() {
 func init() {
 	//初始化GlobalObject变量，设置一些默认值
 	GlobalObject = &GlobalObj{
-		Name:          "ZinxServerApp",
-		Version:       "V0.4",
-		TcpPort:       7777,
-		Host:          "0.0.0.0",
-		MaxConn:       12000,
-		MaxPacketSize: 4096,
+		Name:             "ZinxServerApp",
+		Version:          "V0.4",
+		TcpPort:          6666,
+		Host:             "0.0.0.0",
+		MaxConn:          12000,
+		MaxPacketSize:    4096,
+		ConfFilePath:     "../../conf/zinx.json",
+		WorkerPoolSize:   10,
+		MaxWorkerTaskLen: 1024,
 	}
 
 	//从配置文件中加载一些用户配置的参数
